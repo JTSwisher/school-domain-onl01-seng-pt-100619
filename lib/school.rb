81a6d52a6c5cb843127bc8d6bb4adca3b2397f2e
Jeff Swisher <jefftswisher@gmail.com>
2019-10-21 01:02:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,7 +17,7 @@ class School
   end 
   
   def sort
-      sorted = [roster.sort_by {|n,g| n }]
+      sorted = Hash[roster.sort_by {|n,g| n }]
       sorted.keys.each do |n,g|
         sorted[n].sort!
       end
